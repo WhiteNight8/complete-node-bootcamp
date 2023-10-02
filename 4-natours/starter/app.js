197767d45c761ceabd3c647b8c1c1384a5a8a1a3
Xu <whitenight.xiaxu@gmail.com>
2023-10-02 16:58:25 +0800

    refractor app

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,19 +8,16 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
       tours
     }
   })
-})
+}
 
-const port = 3000
-
-app.post('/api/v1/tours', (req, res) => {
-  // console.log(req.body)
+const CreatNewTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1
   const newTour = Object.assign({ id: newId }, req.body)
 
@@ -38,9 +35,9 @@ app.post('/api/v1/tours', (req, res) => {
       })
     }
   )
-})
+}
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params)
 
   const id = req.params.id * 1
@@ -58,9 +55,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour
     }
   })
-})
+}
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -73,9 +70,8 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: '<Updated tour here...></Updated>'
     }
   })
-})
-
-app.delete('/api/v1/tours/:id', (req, res) => {
+}
+const deleteTour = (req, res) => {
   if (req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -86,8 +82,17 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null
   })
-})
+}
+// app.get('/api/v1/tours', getAllTour)
+// app.post('/api/v1/tours', CreatNewTour)
+// app.get('/api/v1/tours/:id', getTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
 
+app.route('/api/v1/tours').get(getAllTour).post(CreatNewTour)
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
+
+const port = 3000
 app.listen(port, () => {
   console.log(`app runnning on port${port}`)
 })
