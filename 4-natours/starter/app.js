95decf245b6362a5defa85e501293233c64c4116
Xu <whitenight.xiaxu@gmail.com>
2023-10-02 21:26:46 +0800

    refractor app.js

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -17,7 +17,4 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/v1/tours', tourRouter)
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`app runnning on port ${port}`)
-})
+module.exports = app
