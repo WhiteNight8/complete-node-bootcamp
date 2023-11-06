d4e94231af9df2bbbb0aff64b7c454b31469be47
夏 <whitenight2xia@gmail.com>
2023-11-06 11:24:59 +0800

    feat connect mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 dotenv.config({ path: './config.env' })
 
 const app = require('./app')
+const DB = process.env.DATABASE
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then((con) => {
+    console.log(con.connections)
+    console.log('db connection established')
+  })
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
